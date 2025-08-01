8315e5b684c8ba8bc279e29be837fc10d5aa7380
Kyle Christian Gonzales <kcgonzales78193@liceo.edu.ph>
2025-08-01 09:50:33 +0800

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.Scanner;
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter number 1: ");
+            int num1 = input.nextInt();
+
+            System.out.println("Enter number 2: ");
+            int num2 = input.nextInt();
+
+            System.out.println("Enter number 3: ");
+            int num3 = input.nextInt();
+
+            System.out.println("Enter number 4: ");
+            int num4 = input.nextInt();
+
+            System.out.println("Enter number 5: ");
+            int num5 = input.nextInt();
+
+
+            int totalNumber = num1 + num2 + num3 + num4 + num5;
+
+            System.out.println(" ");
+            System.out.println("totalNumber: " +totalNumber);
+
+
+
+
+
+        }
+
 }
