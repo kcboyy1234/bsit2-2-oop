2c0489af3afb9bdc0f4b03b46e7453278f245db6
kcboyy1234 <kcgonzales78193@liceo.edu.ph>
2025-08-08 11:00:56 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student1 = new Student("kyle", 20, "BSIT", 85.0, 90.0, 88.0);
+        Student student2 = new Student("christian", 19, "BSCS", 92.0, 95.0, 89.0);
+        Student student3 = new Student("gonzales", 21, "BSIT", 65.0, 70.0, 68.0);
+
+        Student[] students = {student1, student2, student3};
+        int passingCount = 0;
+
+        for (Student student : students) {
+            student.displayInfo();
+            if (student.isPassing()) {
+                passingCount++;
+            }
+        }
+
+        System.out.println("Summary: " + passingCount + " out of " +
+                students.length + " students are passing.");
     }
 }
